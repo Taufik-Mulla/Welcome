7f92486b5b2f2aebbcf0b66dfd2b49fb99b3329c
Taufik-Mulla <mullataufik0313@gmail.com>
2026-02-28 16:36:29 +0530

    Added print statement

--- src/test/java/com/jbk/LoginPage.java
+++ src/test/java/com/jbk/LoginPage.java
@@ -1,6 +1,7 @@
 package com.jbk;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginPage {
@@ -8,8 +9,13 @@ public class LoginPage {
 	public void Testdta() {
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.saucedemo.com/");
-		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-		driver.findElement(By.id("password")).sendKeys("secret_sauce");
-		driver.findElement(By.id("login-button")).click();
+		WebElement TxtUserName = driver.findElement(By.id("user-name"));
+		TxtUserName.sendKeys("standard_user");
+		WebElement TxtPassword = driver.findElement(By.id("password"));
+		TxtPassword.sendKeys("secret_sauce");
+		WebElement clickbtn = driver.findElement(By.id("login-button"));
+		clickbtn.click();
+		
+		driver.quit();
 	}
 }
